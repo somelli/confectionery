e0a2c54965794992d2def1cae5722c4c0d91cf16
somelli <159928612+somelli@users.noreply.github.com>
2025-02-13 17:40:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.quantity_inner .bt_minus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+});
+$('.quantity_inner .bt_plus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+});
+$('.quantity_inner .quantity').bind("change keyup input click", function () {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+        this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+        this.value = parseInt($(this).data('max-count'));
+    }
+}); 
